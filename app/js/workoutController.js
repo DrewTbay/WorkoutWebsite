3fed37e69488c46a7299246a84646b13e0c17cb7
DrewTbay <drew.tl.reid@gmail.com>
2016-04-26 18:01:36 -0400

    Tinkering with Controllers

--- app/js/workoutController.js
+++ app/js/workoutController.js
@@ -4,19 +4,19 @@ var workoutControllers = angular.module('workoutControllers', []);
 workoutControllers.controller('mainController', ['$scope', '$routeParams',
 	function($scope, $routeParams) {
 		// create a message to display in our view
-		$scope.message = 'Welcome to my Workout server!';
+		$routeParams.message = 'Welcome to my Workout server!';
 	}]);
 
 // create the controller and inject Angular's $scope
 workoutControllers.controller('aboutController', ['$scope', '$routeParams',
 	function($scope, $routeParams) {
 		// create a message to display in our view
-		$scope.message = 'The About Page';
+		$routeParams.message = 'The About Page';
 	}]);
 
 // create the controller and inject Angular's $scope
 workoutControllers.controller('eccentricUpperController', ['$scope', '$routeParams',
 	function($scope, $routeParams) {
-		$scope.message = "This is a test.";
+		$routeParams.message = "This is a test.";
 	}]);
  
