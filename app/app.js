1919b35c80777400f98fabb5fa2c8435950348c0
DrewTbay <drew.tl.reid@gmail.com>
2016-04-26 17:58:50 -0400

    Tinkering with Controllers

--- app/app.js
+++ app/app.js
@@ -2,27 +2,33 @@
 
 // create the module and name it hostApp
 // also include ngRoute for all our routing needs
-var hostApp = angular.module('hostApp', ['ngRoute','workoutControllers']);
+var hostApp = angular.module('hostApp', [
+	'ngRoute',
+	'workoutControllers'
+]);
 
 // configure our routes
-hostApp.config(['$routeProvider', function($routeProvider) {
-	$routeProvider
+hostApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
 
-	// route for the home page
-	.when('/', {
-		templateUrl : 'app/home/home.html',
-		controller : 'mainController'
-	})
-
-	// route for the about page
-	.when('/about', {
-		templateUrl : 'app/about/about.html',
-		controller : 'aboutController'
-	})
-	
-	// route for the Eccentric Upper page
-	.when('/eccentricUpper', {
-		templateUrl : 'app/workouts/eccentricUpper/eccentricUpper.html',
-		controller : 'eccentricUpperController'
-	});
-}]);
+		// route for the home page
+		when('/', {
+			templateUrl : 'app/home/home.html',
+			controller : 'mainController'
+		}).
+		// route for the about page
+		when('/about', {
+			templateUrl : 'app/about/about.html',
+			controller : 'aboutController'
+		}).
+		
+		// route for the Eccentric Upper page
+		when('/eccentricUpper', {
+			templateUrl : 'app/workouts/eccentricUpper/eccentricUpper.html',
+			controller : 'eccentricUpperController'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+	}]);
