29d143e99158c048b2e5474579445418605ea86f
DrewTbay <drew.tl.reid@gmail.com>
2016-04-27 19:15:49 -0400

    Update Eccentric Upper

--- app/js/eccentricUpperController.js
+++ app/js/eccentricUpperController.js
@@ -4,12 +4,35 @@ var eccentricUpperControllers = angular.module('eccentricUpperControllers', []);
 
 // create the controller and inject Angular's $scope
 eccentricUpperControllers.controller('eccentricUpperController', ['$scope', function($scope) {
-	$scope.data = {
+	$scope.standardPushups = {
 		availableOptions: [
-			{id: '1', name: 'Option A'},
-			{id: '2', name: 'Option B'},
-			{id: '3', name: 'Option C'}
+			{id: '1', name: 'One'},
+			{id: '2', name: 'Two'},
+			{id: '3', name: 'Three'},
+			{id: '4', name: 'Four'},
+			{id: '5', name: 'Five'},
+			{id: '6', name: 'Six'},
+			{id: '7', name: 'Seven'},
+			{id: '8', name: 'Eight'},
+			{id: '9', name: 'Nine'},
+			{id: '10', name: 'Ten'}
 		],
-		selectedOption: {id: '3', name: 'Option C'} //This sets the default value of the select in the ui
+		selectedOption: {id: '10', name: 'Ten'}
     };
+	$scope.standardPullUps = {
+		availableOptions: [
+			{id: '1', name: 'One'},
+			{id: '2', name: 'Two'},
+			{id: '3', name: 'Three'},
+			{id: '4', name: 'Four'},
+			{id: '5', name: 'Five'},
+			{id: '6', name: 'Six'},
+			{id: '7', name: 'Seven'},
+			{id: '8', name: 'Eight'},
+			{id: '9', name: 'Nine'},
+			{id: '10', name: 'Ten'}
+		],
+		selectedOption: {id: '8', name: 'Eight'}
+    };
+	
 }]);
