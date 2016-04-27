d0ad73516dc85a2404d11ac4df25adfcffd5d818
DrewTbay <drew.tl.reid@gmail.com>
2016-04-26 20:59:55 -0400

    Controller Change

--- app/js/workoutController.js
+++ app/js/workoutController.js
@@ -4,19 +4,19 @@ var workoutControllers = angular.module('workoutControllers', []);
 workoutControllers.controller('mainController', ['$scope', '$routeParams',
 	function($scope, $routeParams) {
 		// create a message to display in our view
-		$routeParams.message = 'Welcome to my Workout server!';
+		$scope.message = 'Welcome to my Workout server!';
 	}]);
 
 // create the controller and inject Angular's $scope
 workoutControllers.controller('aboutController', ['$scope', '$routeParams',
 	function($scope, $routeParams) {
 		// create a message to display in our view
-		$routeParams.message = 'The About Page';
+		$scope.message = 'The About Page';
 	}]);
 
 // create the controller and inject Angular's $scope
 workoutControllers.controller('eccentricUpperController', ['$scope', '$routeParams',
 	function($scope, $routeParams) {
-		$routeParams.message = "This is a test.";
+		$scope.message = "This is a test.";
 	}]);
  
