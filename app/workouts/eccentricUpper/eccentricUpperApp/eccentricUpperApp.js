18ead9c93338f52272ae3eae4ec1b521647906c8
DrewTbay <drew.tl.reid@gmail.com>
2016-04-26 21:37:09 -0400

    Tinkering with Controllers

--- app/workouts/eccentricUpper/eccentricUpperApp/eccentricUpperApp.js
+++
@@ -1,7 +0,0 @@
-var workoutControllers = angular.module('eccentricUpperController', []);
-
-// create the controller and inject Angular's $scope
-workoutControllers.controller('mainController', function($scope) {
-	// create a message to display in our view
-	$scope.message = 'Welcome to my Workout server!';
-});
